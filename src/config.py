1184de25a9a24ae2fe6044257480613ea2b5ca36
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 01:20:19 +0200

    docs(config): add doc strings

--- src/config.py
+++ src/config.py
@@ -5,7 +5,9 @@ from src.utils import Utils
 
 
 class Config:
-
+    """
+    This class manages the parameters stored on config file
+    """
     def __init__(self):
         config = configparser.ConfigParser()
         Utils.create_file_if_not_exist(Path.get_ini_model_path(), Path.get_ini_path())
