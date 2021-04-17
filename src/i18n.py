60064b7a639afdd7909f8b81fd0f08b8a29184df
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 01:20:26 +0200

    docs(i18n): add doc strings

--- src/i18n.py
+++ src/i18n.py
@@ -4,13 +4,19 @@ from src.path import Path
 
 
 class I18n:
-
+    """
+    This class reads the i18n files and manages i18n string with a dict.
+    """
     def __init__(self, language):
         self.i18n_dict = {}
-        self.language = language
+        self.language = language # The selected language.
         self.read_i18n_file()
 
     def read_i18n_file(self):
+        """
+        This function reads the files of translations and build a dictionary that stores all this data.
+        :return:    nothing.
+        """
         i18n_path = Path.get_i18n_path()
         language_list = []
         with open(i18n_path, newline='') as csvfile:
@@ -26,8 +32,19 @@ class I18n:
                 row_count += 1
 
     def translator(self, key, capitalize=False):
+        """
+        This funtion translates a word to the selected language.
+        :param key:         The key of the word we want to translate.
+        :param capitalize:  If we want to capitalize our word.
+        :return:            THe translation of the word associated to key.
+        """
         selected_translation = self.i18n_dict[key][self.language]
         return selected_translation.capitalize() if capitalize else selected_translation
 
     def colonizer(self, text):
+        """
+        This function formats columns according to language convention.
+        :param text:    The text where we want to add a coluln.
+        :return:        The text with a column.
+        """
         return text + " : " if self.language == "french" else text + ": "
