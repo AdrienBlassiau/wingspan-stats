e04e62d0b21267f32f09460463af7bf746050229
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 01:24:41 +0200

    docs(utils): add doc strings

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,18 @@ import os.path
 
 
 class Utils:
+    """
+    This class stores some useful functions
+    """
 
     @staticmethod
     def create_file_if_not_exist(in_file_path, out_file_path):
+        """
+        This file writes the content of an input file into an output file.
+        :param in_file_path:    The input file path.
+        :param out_file_path:   The output file path.
+        :return:                nothing.
+        """
         file_exists = os.path.isfile(out_file_path)
 
         if not file_exists:
