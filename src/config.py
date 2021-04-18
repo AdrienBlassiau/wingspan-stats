daae0f93ddafe4d0725d1c4716513b0189490dad
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 02:47:09 +0200

    feat(config): add colors managment on config class

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import configparser
 
+from src.color import Color
 from src.path import Path
 from src.utils import Utils
 
@@ -8,9 +9,17 @@ class Config:
     """
     This class manages the parameters stored on config file
     """
+
     def __init__(self):
         config = configparser.ConfigParser()
         Utils.create_file_if_not_exist(Path.get_ini_model_path(), Path.get_ini_path())
         config.read(Path.get_ini_path())
 
         self.language = config.get('DEFAULT', 'Language')
+        self.colors = Color(config.get('COLORS', 'Birds'),
+                            config.get('COLORS', 'BonusCards'),
+                            config.get('COLORS', 'EndOfRoundGoals'),
+                            config.get('COLORS', 'Eggs'),
+                            config.get('COLORS', 'FoodOnCards'),
+                            config.get('COLORS', 'TuckedCardsBar'),
+                            config.get('COLORS', 'NectarsBar'))
