bff031e7bd3193cfca96b847a081b1e96ff87fda
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 15:51:51 +0200

    fix(path): fix path file to work on dev and prod

--- src/path.py
+++ src/path.py
@@ -1,6 +1,6 @@
 import os
 
-from definitions import ROOT_DIR
+from definitions import OUTSIDE_ROOT_DIR, INSIDE_ROOT_DIR
 from src.utils import Utils
 
 
@@ -8,12 +8,13 @@ class Path:
     """
     This class stores all the path.
     """
-    DEFAULT_INPUT_PATH = ROOT_DIR + "/input/"
-    DEFAULT_OUTPUT_PATH = ROOT_DIR + "/output/"
-    DEFAULT_DATA_PATH = ROOT_DIR + "/data/"
-    DEFAULT_INI_PATH = ROOT_DIR + "/"
+    DEFAULT_INPUT_PATH = OUTSIDE_ROOT_DIR + "/input/"
+    DEFAULT_OUTPUT_PATH = OUTSIDE_ROOT_DIR + "/output/"
+    DEFAULT_DATA_PATH = INSIDE_ROOT_DIR + "/data/"
+    DEFAULT_INI_PATH = OUTSIDE_ROOT_DIR + "/"
     DEFAULT_SCORE_OUTPUT_FILE_FORMAT = "score_{player_name}.png"
     DEFAULT_SCORE_FILE = "scores.csv"
+    DEFAULT_SCORE_MODEL_FILE = "scores_model.csv"
     DEFAULT_I18N_FILE = "i18n.csv"
     DEFAULT_INI_FILE = "wingspan.stats.ini"
     DEFAULT_INI_MODEL_FILE = "wingspan.stats.model.ini"
@@ -37,6 +38,15 @@ class Path:
         """
         Utils.create_folder_if_not_exist(Path.DEFAULT_INPUT_PATH)
         return os.path.join(Path.DEFAULT_INPUT_PATH, Path.DEFAULT_SCORE_FILE)
+
+    @staticmethod
+    def get_score_model_path():
+        """
+        This function returns the path where we read the scores model file.
+        :return:                The path where we read the scores model file.
+        """
+        Utils.create_folder_if_not_exist(Path.DEFAULT_DATA_PATH)
+        return os.path.join(Path.DEFAULT_DATA_PATH, Path.DEFAULT_SCORE_MODEL_FILE)
 
     @staticmethod
     def get_i18n_path():
