3d8c1ad988b4a6ab745fbc04b2080c871762ecc5
AdrienBlassiau <adblassiau@gmail.com>
2021-04-18 15:52:51 +0200

    feat(definitions): better path management to compute executable correct path

--- definitions.py
+++ definitions.py
@@ -1,4 +1,13 @@
 import os
+import sys
 
-# The root directory path
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# determine if application is a script file or frozen exe
+
+if getattr(sys, 'frozen', False):
+    INSIDE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    OUTSIDE_ROOT_DIR = os.path.dirname(sys.executable)
+elif __file__:
+    INSIDE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    OUTSIDE_ROOT_DIR = INSIDE_ROOT_DIR
+
+print(INSIDE_ROOT_DIR, OUTSIDE_ROOT_DIR)
